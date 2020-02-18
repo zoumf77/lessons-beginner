c327027613eaaf7487a69739b4462a0a45b5f279
zoumf77 <mfzou@sina.com>
2020-02-18 23:39:40 +0800

    创建

---
+++ src/main/java/com/zoumf77/LogDemo.java
@@ -0,0 +1,11 @@
+package com.zoumf77;
+
+import org.slf4j.*;
+public class LogDemo {
+	
+	 static Logger logger=LoggerFactory.getLogger(LogDemo.class);
+	 
+	 public static void main(String[] args){
+		 logger.info("first logdemo");
+	 }
+}
